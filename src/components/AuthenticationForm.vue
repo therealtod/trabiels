a9033f3e8e391e6faa49db9058978a920ef2b46e
Giunta Eugenio <e.giunta@reply.it>
2020-05-03 17:27:57 +0200

    login form

--- src/components/AuthenticationForm.vue
+++ src/components/AuthenticationForm.vue
@@ -9,93 +9,33 @@
         indicator-color="primary"
         align="justify"
       >
-        <q-tab name="mails" label="Mails" />
-        <q-tab name="alarms" label="Alarms" />
-        <q-tab name="movies" label="Movies" />
+        <q-tab name="login" label="Login" />
+        <q-tab name="registration" label="Registration" />
       </q-tabs>
 
       <q-separator />
 
       <q-tab-panels v-model="tab" animated>
-        <q-tab-panel name="mails" class="q-pa-none">
 
-          <q-splitter
-            v-model="splitterModel"
-            style="height: 250px"
-          >
-
-            <template v-slot:before>
-              <q-tabs
-                v-model="innerTab"
-                vertical
-                class="text-teal"
-              >
-                <q-tab name="innerMails" icon="mail" label="Mails" />
-                <q-tab name="innerAlarms" icon="alarm" label="Alarms" />
-                <q-tab name="innerMovies" icon="movie" label="Movies" />
-              </q-tabs>
-            </template>
-
-            <template v-slot:after>
-              <q-tab-panels
-                v-model="innerTab"
-                animated
-                transition-prev="slide-down"
-                transition-next="slide-up"
-              >
-                <q-tab-panel name="innerMails">
-                  <div class="text-h4 q-mb-md">Mails</div>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Quis praesentium cumque magnam odio iure quidem, quod
-                    illum numquam possimus obcaecati commodi minima assumenda
-                    consectetur culpa fuga nulla ullam. In, libero.</p>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Quis praesentium cumque magnam odio iure quidem, quod illum
-                    numquam possimus obcaecati commodi minima assumenda
-                    consectetur culpa fuga nulla ullam. In, libero.</p>
-                </q-tab-panel>
-
-                <q-tab-panel name="innerAlarms">
-                  <div class="text-h4 q-mb-md">Alarms</div>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                     Quis praesentium cumque magnam odio iure quidem, quod
-                      illum numquam possimus obcaecati commodi minima assumenda
-                       consectetur culpa fuga nulla ullam. In, libero.</p>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                     Quis praesentium cumque magnam odio iure quidem, quod
-                     illum numquam possimus obcaecati commodi minima assumenda
-                      consectetur culpa fuga nulla ullam. In, libero.</p>
-                </q-tab-panel>
-
-                <q-tab-panel name="innerMovies">
-                  <div class="text-h4 q-mb-md">Movies</div>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                     Quis praesentium cumque magnam odio iure quidem, quod
-                      illum numquam possimus obcaecati commodi minima assumenda
-                       consectetur culpa fuga nulla ullam. In, libero.</p>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                     Quis praesentium cumque magnam odio iure quidem, quod
-                      illum numquam possimus obcaecati commodi minima
-                       assumenda consectetur culpa fuga nulla ullam. In,
-                        libero.</p>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                     Quis praesentium cumque magnam odio iure quidem, quod
-                      illum numquam possimus obcaecati commodi minima assumenda
-                       consectetur culpa fuga nulla ullam. In, libero.</p>
-                </q-tab-panel>
-              </q-tab-panels>
-            </template>
-
-          </q-splitter>
+        <q-tab-panel name="login">
+          <div class="text-h6">Login</div>
+          <q-input filled v-model="ph"
+          label="Mail" placeholder="Mail" hint=""
+          :dense="dense" />
+                <q-input v-model="password" label="Password"
+                filled :type="isPwd ? 'password' : 'text'">
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
         </q-tab-panel>
 
-        <q-tab-panel name="alarms">
-          <div class="text-h6">Alarms</div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </q-tab-panel>
-
-        <q-tab-panel name="movies">
-          <div class="text-h6">Movies</div>
+        <q-tab-panel name="registration">
+          <div class="text-h6">Registration</div>
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </q-tab-panel>
       </q-tab-panels>
@@ -112,6 +52,11 @@ export default {
       tab: 'mails',
       innerTab: 'innerMails',
       splitterModel: 20,
+      text: '',
+      ph: '',
+      dense: false,
+      password: '',
+      isPwd: true,
     };
   },
 };
