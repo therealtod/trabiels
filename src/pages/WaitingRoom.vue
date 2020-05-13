579afda500d4b95731306000d83922f36a2cf4e7
Francesco Ippolito <ipp.francesco@gmail.com>
2020-05-13 14:43:25 +0100

    finished Waiting room

--- src/pages/WaitingRoom.vue
+++ src/pages/WaitingRoom.vue
@@ -1,36 +1,46 @@
 <template>
-  <div class="row">
-    <div class="col-12 q-pt-md q-pl-md textHeadBox">
-      Waiting Room (ricordati di mettere pronto, ora!)
-    </div>
-    <div class="col-8 q-pt-xs q-pr-md q-pb-md q-pl-md">
-      <q-table
-        :data="data"
-        :columns="columns"
-        row-key="name"
-        selection="single"
-        :selected.sync="selected"
-        :filter="filter"
-        grid
-        bordered
-        hide-header
-        hide-bottom
-        :pagination="paginationConfig"
-      >
-        <template v-slot:item="props">
-          <div
-            class="q-pa-xs col-xs-12 col-sm-6 col-md-4 col-lg-3 grid-style-transition"
-            :style="props.selected ? 'transform: scale(0.95);' : ''"
+      <div class="row q-py-md q-px-md">
+        <div class="col-12 row justify-around q-pa-md textHeadBox">
+          <q-card class="col-auto q-pa-sm">Waiting Room (ricordati di mettere pronto, ora!)</q-card>
+          <div class="col-4 self-center">
+            <q-btn>Incita a mettere pronto. Premi qui!</q-btn>
+          </div>
+        </div>
+        <div class="col-8 q-pt-xs q-pr-md q-pb-md q-pl-md">
+          <q-table
+            :data="data"
+            :columns="columns"
+            row-key="name"
+            selection="single"
+            :selected.sync="selected"
+            :filter="filter"
+            grid
+            bordered
+            hide-header
+            hide-bottom
+            :pagination="paginationConfig"
           >
-            <q-card
-              :class="props.selected ? 'bg-light-green-12' : ''"
-              :card-style="{ backgroundColor: '#ff0000' }"
-            >
-              <q-card-section>
-                <q-checkbox dense v-model="props.selected" :label="props.row.name" />
-              </q-card-section>
-              <q-list dense>
-                <q-item
+            <template v-slot:item="props">
+              <div
+                class="q-pa-xs col-xs-12 col-sm-6 col-md-4 col-lg-3 grid-style-transition"
+                :style="props.selected ? 'transform: scale(0.95);' : ''"
+              >
+                <q-card
+                  class="row justify-between q-py-lg"
+                  :class="props.selected ? 'bg-light-green-12' : ''"
+                  :card-style="{ backgroundColor: '#ff0000' }"
+                >
+                  <q-card-section class="col-auto">
+                    <q-checkbox dense v-model="props.selected" :label="props.row.name" />
+                  </q-card-section>
+                  <q-list class="col-auto self-center q-mr-md">
+                    <q-btn round>
+                      <q-avatar>
+                        <q-tooltip>Some text as content of Tooltip</q-tooltip>
+                        <img src="https://cdn.quasar.dev/img/avatar.png" />
+                      </q-avatar>
+                    </q-btn>
+                    <!-- <q-item
                   v-for="col in props.cols.filter(col => col.name !== 'desc')"
                   :key="col.name"
                 >
@@ -40,30 +50,33 @@
                   <q-item-section side>
                     <q-item-label caption>{{ col.value }}</q-item-label>
                   </q-item-section>
-                </q-item>
-              </q-list>
-            </q-card>
-          </div>
-        </template>
-      </q-table>
-    </div>
-    <div class="col-4 q-px-md q-py-lg">
-      <q-card class="row" id="statGameCard">
-        <div class="col-12 row items-center">
-          <div class="col-6">Nome Partita:</div>
-          <div class="col-6 text-right">Perchè non sono morto</div>
+                    </q-item>-->
+                  </q-list>
+                </q-card>
+              </div>
+            </template>
+          </q-table>
         </div>
-        <div class="col-12 row items-center">
-          <div class="col-6">Numero Giocatori:</div>
-          <div class="col-6 text-right">8</div>
+        <div class="col-4 q-px-md q-py-md text-h6">
+          <q-card class="row" id="statGameCard">
+            <div class="col-12 row items-center">
+              <div class="col-6 text-weight-regular">Nome Partita:</div>
+              <div class="col-6 text-right">Perchè non sono morto</div>
+            </div>
+            <div class="col-12 row items-center">
+              <div class="col-6 text-weight-regular">Numero Giocatori:</div>
+              <div class="col-6 text-right">8</div>
+            </div>
+            <div class="col-12 row items-center">
+              <div class="col-6 text-weight-regular">Numero Distretti:</div>
+              <div class="col-6 text-right">10</div>
+            </div>
+            <div class="col-12 row justify-around">
+              <div class="col-8 q-ma-sm"></div>
+            </div>
+          </q-card>
         </div>
-        <div class="col-12 row items-center">
-          <div class="col-6">Numero Distretti:</div>
-          <div class="col-6 text-right">10</div>
-        </div>
-      </q-card>
-    </div>
-  </div>
+      </div>
 </template>
 
 <script>
