6a11a1a4352f12d69f4169469d1b13be2fcf0b5c
Francesco Ippolito <ipp.francesco@gmail.com>
2020-05-21 11:03:16 +0100

    layout Home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,16 @@
 <template>
-<div></div>
+<q-page class="backgroundHome q-pt-lg">
+  <q-card class="bodyHome">
+    <div class="text-h5 text-center">Welcome!</div>
+    <div class="text-body1 text-center q-my-sm">
+       Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+       Repellendus quam debitis
+       temporibus veniam sit deleniti eius rem tempora.
+       Tempora dolorem quam excepturi nobis commodi autem,
+       exercitationem voluptatibus explicabo in repellendus!
+    </div>
+  </q-card>
+</q-page>
 </template>
 
 <script>
